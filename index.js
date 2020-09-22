791d4326fde2a45f4b98cd84378ccb52a8c476dc
Edd <ed.gracilla@gmail.com>
2020-09-23 07:05:45 +0800

    fix: reduce 1sec to +24hrs to cover 1 day filter

--- index.js
+++ index.js
@@ -165,7 +165,7 @@ module.exports.prototype.towaBetween = field => (query, value) => {
   if (after.toString() !== 'Invalid Date' && before.toString() !== 'Invalid Date') {
     query[field] = {
       $gte: after,
-      $lte: new Date(before.getTime() + (24 * 3600 * 1000)),
+      $lte: new Date(before.getTime() + ((24 * 3600 * 1000) - 1000)),
     };
   }
 };
